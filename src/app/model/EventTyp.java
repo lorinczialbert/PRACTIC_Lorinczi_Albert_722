01b2d2de6b90ae9a750ffa5e52ac7d476071763e
Albert Lőrinczi <88albertlorinczi@gmail.com>
2026-01-13 08:59:29 +0200

    Implemantat entitati

---
+++ src/app/model/EventTyp.java
@@ -0,0 +1,5 @@
+package app.model;
+
+public enum EventTyp {
+    FOUND_SUPPLIES, INJURED, ATTACK, HELPED_ALLY, SPONSORED
+}
---
+++ src/app/model/Status.java
@@ -0,0 +1,11 @@
+package app.model;
+
+public enum Status {
+    LEBENDIG, TOT;
+
+    public static Status fromJson(String jsonStatus) {
+        if ("ALIVE".equalsIgnoreCase(jsonStatus)) return LEBENDIG;
+        if ("DEAD".equalsIgnoreCase(jsonStatus)) return TOT;
+        return LEBENDIG;
+    }
+}
