b6b8200e3c93dc6f0816e3b142dcb76857e02f76
Albert Lőrinczi <88albertlorinczi@gmail.com>
2026-01-13 09:51:25 +0200

    final

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,12 +6,12 @@ import app.service.HungerGamesService;
 
 public class Main {
     public static void main(String[] args) {
-        // Dependency Injection setup
+
         HungerGamesRepository repository = new HungerGamesRepository();
         HungerGamesService service = new HungerGamesService(repository);
         HungerGamesController controller = new HungerGamesController(service);
 
-        // Start application
+
         controller.run();
     }
 }
